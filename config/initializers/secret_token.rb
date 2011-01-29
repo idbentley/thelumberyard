4ef2451a98110bda68c09672aa9c5d87097ff1c4
idbentley <ian.bentley@gmail.com>
2011-01-29 18:13:18 -0500

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdbentleySite::Application.config.secret_token = '3c44a42d3ebef046f50f1a987639df13dcacf3018c9cfa4d9881daefdf77a99ed0f66e0ed0cbf5f258272824fe9149b82661ef350847747b5b06ab91a2786bda'
