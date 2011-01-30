60ff954d2132e57db05767d8335c4e6eef74a2ee
idbentley <ian.bentley@gmail.com>
2011-01-30 00:06:16 -0500

    commit

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+
+config.gem "jammit" 
+
+
 # Initialize the rails application
 IdbentleySite::Application.initialize!
